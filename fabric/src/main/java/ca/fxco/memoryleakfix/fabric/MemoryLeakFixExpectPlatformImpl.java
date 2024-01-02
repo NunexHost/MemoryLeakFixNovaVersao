57f154873c5f42edbebe0a1c91b71265d37dc1c9
PedrixzZ <131414300+PedrixzZ@users.noreply.github.com>
2024-01-02 15:19:17 -0300

    Update MemoryLeakFixExpectPlatformImpl.java

--- fabric/src/main/java/ca/fxco/memoryleakfix/fabric/MemoryLeakFixExpectPlatformImpl.java
+++ fabric/src/main/java/ca/fxco/memoryleakfix/fabric/MemoryLeakFixExpectPlatformImpl.java
@@ -1,3 +1,5 @@
+package ca.fxco.memoryleakfix.fabric;
+
 import ca.fxco.memoryleakfix.MemoryLeakFixExpectPlatform;
 import net.fabricmc.loader.api.FabricLoader;
 import net.fabricmc.loader.api.Version;
@@ -5,22 +7,18 @@ import net.fabricmc.loader.api.VersionParsingException;
 
 public class MemoryLeakFixExpectPlatformImpl implements MemoryLeakFixExpectPlatform {
 
-    @Override
     public boolean isModLoaded(String id) {
         return FabricLoader.getInstance().isModLoaded(id);
     }
 
-    @Override
     public int compareMinecraftToVersion(String version) throws VersionParsingException {
         return FabricLoader.getInstance().getModContainer("minecraft").get().getMetadata().getVersion().compareTo(Version.parse(version));
     }
 
-    @Override
     public String getMappingType() {
         return "fabric";
     }
 
-    @Override
     public boolean isDevEnvironment() {
         return FabricLoader.getInstance().isDevelopmentEnvironment();
     }
